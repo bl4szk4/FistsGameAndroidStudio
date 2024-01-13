9141aa2289f45f14c3ebcd54a79b43642ff5e692
bl4szk4 <blachabartlomiej@gmail.com>
2024-01-13 13:18:48 +0100

    Generation of squares with random colors and sizes

--- app/src/main/java/com/example/first_game_tutorial/GamePanel.java
+++ app/src/main/java/com/example/first_game_tutorial/GamePanel.java
@@ -4,26 +4,57 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     private Paint redPaint = new Paint();
+    private SurfaceHolder holder;
+    private ArrayList<RndSqare> sqares = new ArrayList<>();
+    private Random random = new Random();
+
     public GamePanel(Context context) {
         super(context);
-        getHolder().addCallback(this);
+        holder = getHolder();
+        holder.addCallback(this);
         redPaint.setColor(Color.RED);
+    }
+
+    private void render(){
+        Canvas c = holder.lockCanvas();
+        c.drawColor(Color.BLACK);
+
+        for(RndSqare square: sqares){
+            square.draw(c);
+
+        }
+
+        holder.unlockCanvasAndPost(c);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            PointF pos = new PointF(event.getX(), event.getY());
+            int color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            int size = 25 + random.nextInt(100);
+            sqares.add(new RndSqare(pos, color, size));
+            render();
+
+        }
+        return true;
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        Canvas c = holder.lockCanvas();
-        c.drawRect(50, 50, 100, 100, redPaint);
-
-        holder.unlockCanvasAndPost(c);
+        render();
     }
 
     @Override
@@ -34,5 +65,20 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
+    }
+
+    private class RndSqare{
+        private PointF p;
+        private int size;
+        private Paint paint;
+        public RndSqare(PointF p, int color, int size){
+            this.p = p;
+            this.size = size;
+            paint = new Paint();
+            paint.setColor(color);
+        }
+        public void draw(Canvas c){
+            c.drawRect(p.x, p.y, p.x + size, p.y + size, paint);
+        }
     }
 }
