b523ee26688bc030794321c9f5e5b9ee29f9e790
bl4szk4 <blachabartlomiej@gmail.com>
2024-02-08 18:37:58 +0100

    created new GameConstants with constant directions of characters, implemented animation for movement and random skeleton movement

--- app/src/main/java/com/example/first_game_tutorial/GamePanel.java
+++ app/src/main/java/com/example/first_game_tutorial/GamePanel.java
@@ -12,6 +12,7 @@ import android.view.SurfaceView;
 import androidx.annotation.NonNull;
 
 import com.example.first_game_tutorial.entities.GameCharacters;
+import com.example.first_game_tutorial.helpers.GameConstants;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -23,41 +24,96 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private Random random = new Random();
     private GameLoop gameLoop;
     private float x, y;
-    private ArrayList<PointF> skeletons = new ArrayList<>();
+//    private ArrayList<PointF> skeletons = new ArrayList<>();
+    private PointF skeletonPos;
+    private int skeletonDir = GameConstants.Face_Dir.LEFT;
 
+    private int playerAniIndexY, playerFaceDir = GameConstants.Face_Dir.DOWN;
+    private int aniTick;
+    private int aniSpeed = 10;
+    private long lastDirChange = System.currentTimeMillis();
     public GamePanel(Context context) {
         super(context);
         holder = getHolder();
         holder.addCallback(this);
         redPaint.setColor(Color.RED);
         gameLoop = new GameLoop(this);
-        for (int i = 0; i < 10; i++)
-            skeletons.add(new PointF(random.nextInt(1080), random.nextInt(1920)));
+        skeletonPos =  new PointF(random.nextInt(1080), random.nextInt(1920));
+//        for (int i = 0; i < 10; i++)
+//            skeletons.add(new PointF(random.nextInt(1080), random.nextInt(1920)));
     }
 
 
     public void render(){
         Canvas c = holder.lockCanvas();
         c.drawColor(Color.BLACK);
-        c.drawBitmap(GameCharacters.SKELETON.getSprite(1, 1), x, y, null);
-        for (PointF p: skeletons)
-            c.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), p.x, p.y, null);
+        c.drawBitmap(GameCharacters.PLAYER.getSprite(playerAniIndexY, playerFaceDir), x, y, null);
+        c.drawBitmap(GameCharacters.SKELETON.getSprite(playerAniIndexY, skeletonDir), skeletonPos.x, skeletonPos.y, null);
+//        for (PointF p: skeletons)
+//            c.drawBitmap(GameCharacters.SKELETON.getSprite(0,0), p.x, p.y, null);
 
         holder.unlockCanvasAndPost(c);
     }
     public void update(double delta){
-        for (PointF p: skeletons) {
-            p.y += delta * 300;
+        if(System.currentTimeMillis() - lastDirChange >= 5000){
+            lastDirChange = System.currentTimeMillis();
+            skeletonDir = random.nextInt(4);
+        }
+        switch (skeletonDir){
+            case GameConstants.Face_Dir.DOWN:
+                skeletonPos.y += delta * 300;
+                if(skeletonPos.y >= 1920)
+                    skeletonDir = GameConstants.Face_Dir.UP;
+                break;
+            case GameConstants.Face_Dir.UP:
+                skeletonPos.y -= delta * 300;
+                if(skeletonPos.y <= 0)
+                    skeletonDir = GameConstants.Face_Dir.DOWN;
+                break;
+            case GameConstants.Face_Dir.LEFT:
+                skeletonPos.x -= delta * 300;
+                if(skeletonPos.x <= 0)
+                    skeletonDir = GameConstants.Face_Dir.RIGHT;
+                break;
+            case GameConstants.Face_Dir.RIGHT:
+                skeletonPos.x += delta * 300;
+                if(skeletonPos.x >= 1080)
+                    skeletonDir = GameConstants.Face_Dir.LEFT;
+                break;
+        }
+        updateAnimation();
+    }
 
-            if (p.y > 1920)
-                p.y = 0;
+    private void updateAnimation(){
+        aniTick++;
+        if(aniTick >= aniSpeed){
+            aniTick = 0;
+            playerAniIndexY++;
+            if(playerAniIndexY >= 4)
+                playerAniIndexY = 0;
         }
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
-            x = event.getX();
-            y = event.getY();
+
+            float newX = event.getX();
+            float newY = event.getY();
+
+            float xDiff = Math.abs(newX - x);
+            float yDiff = Math.abs(newY - y);
+
+            if(xDiff > yDiff) {
+                if (newX > x)
+                    playerFaceDir = GameConstants.Face_Dir.RIGHT;
+                else playerFaceDir = GameConstants.Face_Dir.LEFT;
+            }else{
+                if(newY > y)
+                    playerFaceDir = GameConstants.Face_Dir.DOWN;
+                else playerFaceDir = GameConstants.Face_Dir.UP;
+            }
+            x = newX;
+            y = newY;
         }
         return true;
     }
---
+++ app/src/main/java/com/example/first_game_tutorial/helpers/GameConstants.java
@@ -0,0 +1,10 @@
+package com.example.first_game_tutorial.helpers;
+
+public final class GameConstants {
+    public static final class Face_Dir{
+        public static final int RIGHT = 3;
+        public static final int LEFT = 2;
+        public static final int UP = 1;
+        public static final int DOWN = 0;
+    }
+}
