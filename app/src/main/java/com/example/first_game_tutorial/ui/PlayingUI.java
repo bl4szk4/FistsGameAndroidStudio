b7873c9415231cb69a1e43da9e815ff2c196d2b3
bl4szk4 <blachabartlomiej@gmail.com>
2024-02-26 22:05:46 +0100

    Multitouch handling - first steps

--- app/src/main/java/com/example/first_game_tutorial/ui/PlayingUI.java
+++ app/src/main/java/com/example/first_game_tutorial/ui/PlayingUI.java
@@ -14,9 +14,12 @@ public class PlayingUI {
     //For UI
     private float xCenter = 250, yCenter = 700, radius = 150;
     private Paint circlePaint;
-    private float xTouch, yTouch;
-    private boolean touchDown;
     private CustomButton btnMenu;
+
+    // For multitouch
+    private boolean touchDown;
+    private int joystickPointerId = -1;
+
     public PlayingUI(Playing playing) {
         this.playing = playing;
         circlePaint = new Paint();
@@ -40,49 +43,74 @@ public class PlayingUI {
         drawUI(c);
     }
 
+    private boolean checkInsideJoystick(PointF eventPos, int pointerId){
+        float a = Math.abs(eventPos.x - xCenter);
+        float b = Math.abs(eventPos.y - yCenter);
+        float c = (float)Math.hypot(a, b);
+
+        if(c<= radius){
+            touchDown = true;
+            joystickPointerId = pointerId;
+            return true;
+        }
+        return false;
+    }
+
     public void touchEvents(MotionEvent event){
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                float x = event.getX();
-                float y = event.getY();
+        final int action = event.getActionMasked();
+        final int actionIndex = event.getActionIndex();
+        final int pointerId = event.getPointerId(actionIndex);
+        final PointF eventPos = new PointF(event.getX(actionIndex), event.getY(actionIndex));
 
-                float a = Math.abs(x - xCenter);
-                float b = Math.abs(y - yCenter);
-                float c = (float)Math.hypot(a, b);
+        switch (action){
+            case MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN ->{
 
-                if(c<= radius){
+                if(checkInsideJoystick(eventPos, pointerId)){
                     touchDown = true;
-                    xTouch = x;
-                    yTouch = y;
+
                 }
                 else{
                     if(isIn(event, btnMenu))
                         btnMenu.setPushed(true);
 
                 }
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if(touchDown){
-                    xTouch = event.getX();
-                    yTouch = event.getY();
+            }
+            case MotionEvent.ACTION_MOVE ->{
+                if(touchDown)
+                    for(int i = 0; i < event.getPointerCount(); i++){
+                        if (event.getPointerId(i) == joystickPointerId){
+                            float xDiff = event.getX(i) - xCenter;
+                            float yDiff = event.getY(i) - yCenter;
+                            playing.setPlayerMoveTrue(new PointF(xDiff, yDiff));
+                        }
+                    }
+            }
 
-                    float xDiff = xTouch - xCenter;
-                    float yDiff = yTouch - yCenter;
-
-                    playing.setPlayerMoveTrue(new PointF(xDiff, yDiff));
+            case MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> {
+                if (pointerId == joystickPointerId){
+                    resetJoystickButton();
                 }
-                break;
-            case MotionEvent.ACTION_UP:
-                touchDown = false;
-                playing.setPlayerMoveFalse();
-                if(isIn(event, btnMenu))
-                    if (btnMenu.isPushed())
-                        playing.setGameStateToMenu();
-                btnMenu.setPushed(false);
-                break;
+                else{
+                    if(isIn(event, btnMenu)) {
+                        if (btnMenu.isPushed()) {
+                            resetJoystickButton();
+                            playing.setGameStateToMenu();
+                        }
+                    }
+                    btnMenu.setPushed(false);
+
+                }
+
+            }
+
         }
     }
     private boolean isIn(MotionEvent e, CustomButton b){
         return b.getHitbox().contains(e.getX(), e.getY());
+    }
+    private void resetJoystickButton(){
+        touchDown = false;
+        playing.setPlayerMoveFalse();
+        joystickPointerId = -1;
     }
 }
